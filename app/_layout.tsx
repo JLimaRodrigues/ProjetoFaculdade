11b6db6fa35bc22b4714d95aa441d35dbe8469d5
lima.rodrigues <limarodrigues.silva@eb.mil.br>
2024-10-22 16:09:18 -0300

    feat: criando tela de login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { Button } from "react-native";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+export default function Layout() {
+    return (
+        <AuthProvider>
+            <AuthStack />
+        </AuthProvider>
+    );
 }
+
+const AuthStack = () => {
+    const { authState, onLogout } = useAuth();
+
+    return (
+        <Stack>
+            {authState?.authenticated ? (
+                <Stack.Screen 
+                    name="screens/Home" 
+                    options={{
+                        headerRight: () => <Button onPress={onLogout} title="Sign Out" />
+                    }}
+                />
+            ) : (
+                <Stack.Screen 
+                    name="screens/Login"
+                />
+            )}
+        </Stack>
+    );
+};
