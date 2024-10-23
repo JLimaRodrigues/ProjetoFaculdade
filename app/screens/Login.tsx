d85936d32476dc90c776f6ea8dff145f3b639a79
lima.rodrigues <limarodrigues.silva@eb.mil.br>
2024-10-23 16:28:36 -0300

    resolvendo o problema de redirecionamento no app

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
-import React, { useState } from "react";
+import { View, TextInput, Button } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
@@ -7,9 +7,37 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const { onLogin, onRegister } = useAuth()
 
+    useEffect(() => {
+        const testCall = async () => {
+            const result = await axios.get(`${API_URL}/users`)
+        }
+        testCall()
+    }, [])
+
+    const login = async () => {
+        const result = await onLogin!(email, password)
+        if(result && result.error){
+            alert(result.msg)
+        }
+    }
+
+    const register = async () => {
+        const result = await onRegister!(email, password)
+        if(result && result.error){
+            alert(result.msg)
+        } else {
+            login()
+        }
+    }
+
     return (
         <View>
-            <Text>Login</Text>
+            <View>
+                <TextInput placeholder="Email" onChangeText={(text: string) => setEmail(email)} value={email} />
+                <TextInput placeholder="Password" secureTextEntry={true} onChangeText={(text: string) => setPassword(password)} value={password} />
+                <Button onPress={login} title="Sign in" />
+                <Button onPress={register} title="Create Account" />
+            </View>
         </View>
     )
 }
