b54bd4d3f67c729318c546171a19288d17f16464
JLimaRodrigues <jeffersonlima147@hotmail.com>
2024-12-11 22:49:27 -0300

    feat: estilizando o marcador da localização do usuário

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -4,6 +4,7 @@ import { View, StyleSheet, Text, Alert } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 import * as Location from 'expo-location';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
 
@@ -67,6 +68,18 @@ export default function HomeScreen() {
           : DEFAULT_REGION}
         onPress={handleMapPress}
       >
+        {location && (
+          <Marker 
+            style={styles.markerCircle}
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude
+            }}
+            description={"Localização do Usuário"}
+          >
+            <Ionicons name={'person-sharp'} style={styles.markerIcon} size={20} />
+          </Marker>
+        )}
         {selectedLocation && (
           <Marker 
             coordinate={{
@@ -109,5 +122,21 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
     fontSize: 16,
+  },
+  markerCircle: {
+    backgroundColor: 'green',
+    borderRadius: 30 / 2, // Ajustado para um círculo perfeito
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5, // Adiciona sombra para um efeito 3D
+  },
+  markerIcon: {
+    color: 'white',
   },
 });
